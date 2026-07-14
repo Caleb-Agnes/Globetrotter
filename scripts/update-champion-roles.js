54cf0e7c1975ae79646072e1960c874738be4774
Caleb <Caleb.John.Agnes@gmail.com>
2026-07-14 20:17:37 +0100

    small adjustment to logging and sample size

--- scripts/update-champion-roles.js
+++ scripts/update-champion-roles.js
@@ -13,7 +13,7 @@ const PLATFORM_ROUTE = "euw1"; //used for league-v4 (rank lookups)
 const REGIONAL_ROUTE = "europe"; //used for match-v5 (match id and match detail lookups)
 const MAX_PAGE = 20; //decides the most pages that a player can be randomly sleceted from
 const DATASET_MAX_AGE_MS = 14 * 24 * 60 * 60 * 1000; //2 weeks - a completed dataset older than this gets reset
-const MAX_API_CALLS = 1000; // 1000 calls took ~12 minutes
+const MAX_API_CALLS = 2500; // 1000 calls took ~12 minutes
 
 const roleNames = { TOP: "top", JUNGLE: "jng", MIDDLE: "mid", BOTTOM: "bot", UTILITY: "sup" };
 
@@ -155,9 +155,8 @@ async function riotFetch(url, errorContext) {
     apiAccessCount++;
     //log every sso many apri calls to mark progress when watching the log live
     if (apiAccessCount % 100 === 0) {
-        log('-----------------------------------------------------------');
         log(`${Math.round(apiAccessCount / MAX_API_CALLS * 100)}% THROUGH (${apiAccessCount}/${MAX_API_CALLS} API CALLS)`);
-        log('-----------------------------------------------------------');
+        log('----------------');
     }
     if (response.status === 429) {
         //rate limited so waitrs the instructed time before trying again
@@ -262,6 +261,8 @@ function updateChampionTallies(matchInfo) {
             if (!frequencyTable[championId].done) {
                 updateDoneForChampion(championId);
             }
+        } else {
+            log('X');
         }
     });
     log('----------------');
